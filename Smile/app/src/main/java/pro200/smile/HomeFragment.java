468a1dcc9aed7dd491c107f4f22f9a387f22fcfb
Giftasaurus <ngift@student.neumont.edu>
2018-05-16 14:55:19 -0600

    Created the project, setup the bottom nav bar and fragments for each tab.

---
+++ Smile/app/src/main/java/pro200/smile/HomeFragment.java
@@ -0,0 +1,34 @@
+package pro200.smile;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class HomeFragment extends Fragment {
+
+    public static Fragment newInstance() {
+        Fragment frag = new HomeFragment();
+        return frag;
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        if (savedInstanceState == null) {
+            Bundle args = getArguments();
+        }
+    }
+}
---
+++ Smile/app/src/main/java/pro200/smile/MainActivity.java
@@ -0,0 +1,64 @@
+package pro200.smile;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String SELECTED_ITEM = "navigation_home";
+    private BottomNavigationView mBottomNav;
+    private int mSelectedItemInt;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            selectFragment(item);
+            return true;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mBottomNav = (BottomNavigationView) findViewById(R.id.navigation);
+        mBottomNav.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        MenuItem selectedItem = mBottomNav.getMenu().getItem(0);
+        selectFragment(selectedItem);
+    }
+
+    private void selectFragment(MenuItem item) {
+        Fragment frag = null;
+
+        // update selected item
+        mSelectedItemInt = item.getItemId();
+
+        // init corresponding fragment
+        switch (item.getItemId()) {
+            case R.id.navigation_home:
+                frag = HomeFragment.newInstance();
+                break;
+            case R.id.navigation_smile:
+                frag = SmileFragment.newInstance();
+                break;
+            case R.id.navigation_profile:
+                frag = ProfileFragment.newInstance();
+                break;
+        }
+
+        if (frag != null) {
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.replace(R.id.container, frag, frag.getTag());
+            ft.commit();
+        }
+    }
+}
